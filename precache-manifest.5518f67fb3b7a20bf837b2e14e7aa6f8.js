dc0db66a42f6535856797f3e50840c8644c62eaf
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-04-23 00:12:11 -0300

    Updates

---
+++ precache-manifest.5518f67fb3b7a20bf837b2e14e7aa6f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e866d3f956e5c0e00e1e9b5b864b2e8",
+    "url": "/github-repositories/index.html"
+  },
+  {
+    "revision": "e05addbfdc3e1894a84d",
+    "url": "/github-repositories/static/js/2.0016cd19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-repositories/static/js/2.0016cd19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bc2e60783b724f2774",
+    "url": "/github-repositories/static/js/main.c3b681ef.chunk.js"
+  },
+  {
+    "revision": "99d453c6514fae1110dd",
+    "url": "/github-repositories/static/js/runtime-main.7432ee8c.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-repositories/static/media/github-bg.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-repositories/static/media/github-logo.04f6798f.svg"
+  }
+]);
